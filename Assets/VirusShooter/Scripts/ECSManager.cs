6e5f2acfea2b8bd358508e111c6f1165274b13b0
Alexander Shkurlatov <shkurlatov80@gmail.com>
2023-05-18 06:02:14 +1200

    Add ECS manager

---
+++ Assets/VirusShooter/Scripts/ECSManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class ECSManager : MonoBehaviour
+{
+    public static EntityManager manager;
+    public GameObject virusPrefab;
+
+    int numVirus = 500;
+    BlobAssetStore store;
+
+    void Start()
+    {
+        store = new BlobAssetStore();
+        manager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        var settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, store);
+        Entity virus = GameObjectConversionUtility.ConvertGameObjectHierarchy(virusPrefab, settings);
+
+        for (int i = 0; i < numVirus; i++)
+        {
+            var instance = manager.Instantiate(virus);
+            float x = UnityEngine.Random.Range(-50, 50);
+            float y = UnityEngine.Random.Range(-50, 50);
+            float z = UnityEngine.Random.Range(-50, 50);
+
+            float3 position = new float3 (x, y, z);
+            manager.SetComponentData(instance, new Translation { Value = position });
+        }
+    }
+
+    void OnDestroy()
+    {
+        store.Dispose();
+    }
+}
