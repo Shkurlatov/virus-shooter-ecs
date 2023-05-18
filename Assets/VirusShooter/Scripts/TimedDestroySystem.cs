143f3e61d871536075084a1dab1dfc438977c7ce
Alexander Shkurlatov <shkurlatov80@gmail.com>
2023-05-19 05:49:08 +1200

    Add Timed destroy system

---
+++ Assets/VirusShooter/Scripts/TimedDestroySystem.cs
@@ -0,0 +1,32 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Jobs;
+using Unity.Collections;
+using Unity.Physics;
+
+public class TimedDestroySystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        float dT = Time.DeltaTime;
+
+        Entities
+            .WithoutBurst()
+            .WithStructuralChanges()
+            .ForEach((Entity entity,
+                        ref Translation position,
+                        ref LifetimeData lifetimeData) =>
+            {
+                lifetimeData.LifeLeft -= dT;
+
+                if (lifetimeData.LifeLeft <= 0f)
+                {
+                    EntityManager.DestroyEntity(entity);
+                }
+            })
+            .Run();
+
+        return inputDeps;
+    }
+}
